093ff8d5bee0594fd49d059baf8ee31ee7f0793d
turazi <dtang4@stevens.edu>
2019-11-17 13:10:53 -0500

    Pushed project files

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  console.log("testing");
+
+  $(".navigation__link").on("click", function(event) {
+    $(".navigation__nav").css("opacity", "0");
+    $(".navigation__nav").css("width", "0");
+    $(".navigation__background").css("transform", "scale(0)");
+    $(".navigation__checkbox").prop("checked", false);
+    $(".navigation__nav").removeAttr("style");
+    $(".navigation__background").removeAttr("style");
+  });
+});
